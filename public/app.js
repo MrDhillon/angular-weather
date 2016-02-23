a6eefe6ac70a5459ca7c357ba21745298c212a8f
MrDhillon <harman.singh.dhillon@gmail.com>
2016-02-23 01:28:16 -0500

    displaying formatted weather data, specify days with routeparams

--- public/app.js
+++ public/app.js
@@ -15,6 +15,10 @@ weatherApp.config(function($routeProvider){
     templateUrl:  'views/forecast.html',
     controller: 'forecastController'
   })
+  .when('/forecast/:days',{
+    templateUrl:  'views/forecast.html',
+    controller: 'forecastController'
+  })
 
 });
 // CUSTOM SERVICES
@@ -29,13 +33,20 @@ weatherApp.controller('mainController',['$scope','cityService',function($scope,c
   })
 }]);
 
-weatherApp.controller('forecastController',['$scope','$resource','cityService',function($scope,$resource,cityService){
+weatherApp.controller('forecastController',['$scope','$resource','$routeParams','cityService',function($scope,$resource,$routeParams,cityService){
 
   $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || 2;
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{
     callback: "JSON_CALLBACK"},{ get: {method: "JSONP"}});
 
-    $scope.weatherResult =  $scope.weatherAPI.get({q: $scope.city, cnt: 2, appid: '44db6a862fba0b067b1930da0d769e98'});
-    console.log($scope.weatherResult);
+    $scope.weatherResult =  $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, appid: '44db6a862fba0b067b1930da0d769e98'});
+    $scope.convertToCelsius = function(degC){
+      return Math.round(degC - 273.15);
+    };
+    $scope.convertToDate = function(dt){
+      return new Date(dt * 1000);
+    };
 }]);
